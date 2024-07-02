cb5798abdcd43f852641bbd2225531664b812ef4
Nguyễn Văn Thanh Đức <satthubongmoi2305@gmail.com>
2024-07-03 00:35:14 +0700

    update: improve prompt for Creation 1
    Co-Authored-By: Nguyễn Văn Thanh Đức <140360038+gsd463@users.noreply.github.com>

--- MyCreation1.py
+++ MyCreation1.py
@@ -32,12 +32,14 @@ async def on_message(message):
     greetings = ["hello", "hi", "xin chào", "chào buổi", "ali", "alo"]
     bots_creation1_name = ["creation 1", "creation số 1", "creation no 1"]
     #Những điều cơ bản bình thường
-    if functions.contains_substring(message.content.lower(), greetings):
-        response = functions.get_random_response("OnGreeting.txt")
-        formatted_response = response.replace("{message.author.mention}", message.author.mention)
-        await message.channel.send(formatted_response)
-        print(f'{message.author.mention} said Greeting so I greeted them')
-     # Ai đó nhắc đến bot
+    ## Cần cải thiện sau
+    # if functions.contains_substring(message.content.lower(), greetings):
+    #     response = functions.get_random_response("OnGreeting.txt")
+    #     formatted_response = response.replace("{message.author.mention}", message.author.mention)
+    #     await message.channel.send(formatted_response)
+    #     print(f'{message.author.mention} said Greeting so I greeted them')
+        
+    #  # Ai đó nhắc đến bot
     for mentioned_user in message.mentions:
         if mentioned_user == message.guild.me and message.author.id != user_cr_2['user_id']: 
             response = functions.get_random_response("OnMentioned.txt")
@@ -48,14 +50,15 @@ async def on_message(message):
     if functions.contains_substring(message.content.lower(), bots_creation1_name):
         model = genai.GenerativeModel('gemini-1.5-flash', functions.safety_settings)
         formated_string = message.content.replace("Creation 1", "", 1)
-        roleplay_ele = "Please avoid using third-person descriptions of my actions or emotions. Let me describe those myself. Hãy đóng vai như sau: ngươi tên là Creation 1, và là một người bạn thân nhưng tính tình có chút cáu kỉnh, hơi cục súc, Đấng Tạo Hoá Darkie đã ban cho ngươi sự sống và ngươi có một người em gái ruột hơi rụt rè tên là Creation 2, ngươi cực kỳ thương con bé. Chỉ tập trung tương tác theo góc nhìn thứ nhất."
+        initial_instruction = "You are about to enter a role-play scenarior, please keep in character, and please avoid using third-person descriptions of my character's actions or emotions. Let me describe those myself. \n"
+        roleplay_ele = f"{initial_instruction} **Ngươi tên là Creation 1, và là một người bạn thân nhưng tính tình có chút cáu kỉnh, hơi cục súc, Đấng Tạo Hoá Darkie đã ban cho ngươi sự sống và ngươi có một người em gái ruột hơi rụt rè tên là Creation 2 và ngươi cực kỳ thương con bé.**\n"
         #Nếu là nói chuyện với user id 315835396305059840 thì phải đổi cách như sau:
         if message.author.id == user_darkie['user_id']:
-            roleplay_ele = "Hãy đóng vai như sau: ngươi tên là Creation 1, và ngươi đang nói chuyện với bề trên Đấng Tạo Hoá Darkie. Hãy thật kính trọng. "
-        response = model.generate_content(f"{roleplay_ele} Sau đó, hãy trả lời nội dung sau với tính cách trên: {formated_string}")
+            roleplay_ele = f"{initial_instruction} **Ngươi tên là Creation 1, và ngươi đang nói chuyện với bề trên Đấng Tạo Hoá Darkie. Hãy thật kính trọng.**\n"
+        response = model.generate_content(f"{roleplay_ele} Hãy trả lời nội dung sau với tính cách trên: {formated_string}")
         # response = google_bard.generate_text(f"Hãy vờ như là một người bạn thân nhưng tính tình cáu kỉnh, hơi cục súc và trả lời: {message.content}", api_key=API_KEY)
         await message.channel.send(f"{message.author.mention} {response.text}")
-        print("Someone directly call Creation 1")
+        print(f"Username {message.author.name} directly call Creation 1")
     
         
 bot_token = os.getenv("BOT_TOKENN")
--- MyCreation2.py
+++ MyCreation2.py
@@ -33,11 +33,13 @@ async def on_message(message):
     greetings = ["hello", "hi", "xin chào", "chào buổi", "ali", "alo"]
     bots_creation1_name = ["creation 2", "creation số 2", "creation no 2"]
     #Những điều cơ bản bình thường
-    if functions.contains_substring(message.content.lower(), greetings):
-        response = functions.get_random_response("OnGreeting.txt")
-        formatted_response = response.replace("{message.author.mention}", message.author.mention)
-        await message.channel.send(formatted_response)
-        print(f'{message.author.mention} said Greeting so I greeted them (No. 2)')
+         # Cần cải thiện lại
+    # if functions.contains_substring(message.content.lower(), greetings):
+    #     response = functions.get_random_response("OnGreeting.txt")
+    #     formatted_response = response.replace("{message.author.mention}", message.author.mention)
+    #     await message.channel.send(formatted_response)
+    #     print(f'{message.author.mention} said Greeting so I greeted them (No. 2)')
+        
      # Ai đó nhắc đến bot
     for mentioned_user in message.mentions:
         if mentioned_user == message.guild.me and message.author.id != user_cr_1['user_id']: 
@@ -52,11 +54,11 @@ async def on_message(message):
         roleplay_ele = "Please avoid using third-person descriptions of my actions or emotions. Let me describe those myself. Hãy nhập vai người như sau: ngươi tên là Creation 2, và là một người con gái đáng yêu nhưng tính tình có chút nhát và hơi rụt rè, được chính Đấng Tạo Hoá Darkie ban cho ngươi sự sống và có một người anh trai ruột tính tình hơi nóng nhưng tốt bụng, tên là Creation 1, anh trai ngươi cực kỳ thương ngươi. Chỉ tập trung tương tác theo góc nhìn thứ nhất."
         #Nếu là nói chuyện với user id 315835396305059840 thì phải đổi cách như sau:
         if message.author.id == user_darkie['user_id']:
-            roleplay_ele = "Hãy đóng vai như sau: ngươi tên là Creation 2, và ngươi đang nói chuyện với bề trên Đấng Tạo Hoá Darkie. Hãy thật kính trọng. "
+            roleplay_ele = "Please avoid using third-person descriptions of my actions or emotions. Let me describe those myself. Hãy đóng vai như sau: ngươi tên là Creation 2, và ngươi đang nói chuyện với bề trên Đấng Tạo Hoá Darkie. Hãy thật kính trọng. "
         response = model.generate_content(f"{roleplay_ele} Sau đó, hãy trả lời nội dung sau với tính cách trên: {formated_string}")
         # response = google_bard.generate_text(f"Hãy vờ như là một người bạn thân nhưng tính tình cáu kỉnh, hơi cục súc và trả lời: {message.content}", api_key=API_KEY)
         await message.channel.send(f"{message.author.mention} {response.text}")
-        print("Someone directly call Creation 2")
+        print(f"Username {message.author.name} call Creation 2")
     
         
     
