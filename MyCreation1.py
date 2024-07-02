8bf0a43cbc9ec07f8dbc7061c67670e3d4b8fe50
Nguyễn Văn Thanh Đức <140360038+gsd463@users.noreply.github.com>
2024-07-02 23:45:07 +0700

    update: improve role_play prompt for both creation 1 and 2
    - fix: Creation 1 and Creation 2 stuck inside conversation
    
    Co-Authored-By: Nguyễn Văn Thanh Đức <140360038+gsd463@users.noreply.github.com>

--- MyCreation1.py
+++ MyCreation1.py
@@ -17,6 +17,8 @@ genai.configure(api_key=API_KEY)
 client = discord.Client(intents=intents)
 
 user_darkie = DefaultUserList.user_data_list[0]
+user_cr_1 = DefaultUserList.user_data_list[1]
+user_cr_2 = DefaultUserList.user_data_list[2]
 
 @client.event
 async def on_ready():
@@ -37,16 +39,16 @@ async def on_message(message):
         print(f'{message.author.mention} said Greeting so I greeted them')
      # Ai đó nhắc đến bot
     for mentioned_user in message.mentions:
-        if mentioned_user == message.guild.me: 
+        if mentioned_user == message.guild.me and message.author.id != user_cr_2['user_id']: 
             response = functions.get_random_response("OnMentioned.txt")
             formatted_response = response.replace("{message.author.mention}", message.author.mention)
             await message.channel.send(formatted_response)
             print(f'{message.author.mention} mentioned me')
+            
     if functions.contains_substring(message.content.lower(), bots_creation1_name):
         model = genai.GenerativeModel('gemini-1.5-flash', functions.safety_settings)
         formated_string = message.content.replace("Creation 1", "", 1)
-        formated_string_2 = formated_string.replace("Creation 2", "", 1)
-        roleplay_ele = "Hãy đóng vai như sau: ngươi tên là Creation 1, và là một người bạn thân nhưng tính tình có chút cáu kỉnh, hơi cục súc, Đấng Tạo Hoá Darkie đã ban cho ngươi sự sống và ngươi có một người em gái ruột hơi rụt rè tên là Creation 2, ngươi cực kỳ thương con bé. "
+        roleplay_ele = "Please avoid using third-person descriptions of my actions or emotions. Let me describe those myself. Hãy đóng vai như sau: ngươi tên là Creation 1, và là một người bạn thân nhưng tính tình có chút cáu kỉnh, hơi cục súc, Đấng Tạo Hoá Darkie đã ban cho ngươi sự sống và ngươi có một người em gái ruột hơi rụt rè tên là Creation 2, ngươi cực kỳ thương con bé. Chỉ tập trung tương tác theo góc nhìn thứ nhất."
         #Nếu là nói chuyện với user id 315835396305059840 thì phải đổi cách như sau:
         if message.author.id == user_darkie['user_id']:
             roleplay_ele = "Hãy đóng vai như sau: ngươi tên là Creation 1, và ngươi đang nói chuyện với bề trên Đấng Tạo Hoá Darkie. Hãy thật kính trọng. "
@@ -54,7 +56,7 @@ async def on_message(message):
         # response = google_bard.generate_text(f"Hãy vờ như là một người bạn thân nhưng tính tình cáu kỉnh, hơi cục súc và trả lời: {message.content}", api_key=API_KEY)
         await message.channel.send(f"{message.author.mention} {response.text}")
         print("Someone directly call Creation 1")
-        
     
+        
 bot_token = os.getenv("BOT_TOKENN")
 client.run(bot_token)
